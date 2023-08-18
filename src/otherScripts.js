93c222bda3de63fe86880d92cd4dbc69827d5764
gene courtney <genecourtney7@gmail.com>
2023-08-17 23:01:30 -0500

    made a succesfull api call

--- src/otherScripts.js
+++ src/otherScripts.js
@@ -1,2 +1,19 @@
 //business logic
-export default class
+class ExchangeRateAPI {
+    constructor(apiKey) {
+      this.apiKey = apiKey;
+      this.endpoint = `https://v6.exchangerate-api.com/v6/${this.apiKey}/latest/USD`;
+    }
+  
+    async getRates() {
+      const response = await fetch(this.endpoint);
+      const data = await response.json();
+      return data;
+    }
+  }
+  
+  const exchangeRateAPI = new ExchangeRateAPI('process.env.API_key');
+  exchangeRateAPI.getRates().then(data => {
+    console.log(data);
+  });
+  
